557127c55849c7eecffa62513ba5595dd56e5201
MalluCortez <maria.santana@outlook.com.br>
2023-01-12 14:28:39 -0300

    requisito 4 feito parcialmente, falta 20%

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import playerReducer from './player';
 
-const rootReducer = combineReducers({ playerReducer });
+const rootReducer = combineReducers({ player: playerReducer });
 
 export default rootReducer;
